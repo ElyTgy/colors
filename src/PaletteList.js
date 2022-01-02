16221383759f8a440a7c2a89ff72744636b05964
Ellie <ytaghavi@yahoo.com>
2022-01-02 15:21:36 -0800

    mini palettes

--- src/PaletteList.js
+++ src/PaletteList.js
@@ -1,19 +1,63 @@
 import React, { Component } from 'react'
 import {Link} from 'react-router-dom'
+import MiniPalette from './MiniPalette'
+import {withStyles} from "@material-ui/styles"
 
-export default class PaletteList extends Component {
+const styles = {
+    root: {
+        backgroundColor: "blue",
+        height: "100%",
+        display: "flex",
+        alignItems: "flex-start",
+        justifyContent: "center"
+    },
+    container: {
+        width:"65%",
+        display:"flex",
+        alignItems:"flex-start",
+        flexDirextion: "column",
+        flexWrap:"wrap"
+    },
+    nav:{
+        display:"flex",
+        width:"100%",
+        justifyContent:"space-between",
+        color: "white"
+    },
+    palettes: {
+        boxSizing:"border-box",
+        width:"100%",
+        display:"grid",
+        gridTemplateColumns:"repeat(3, 30%)",
+        gridGap: "5%"
+    }
+}
+
+
+class PaletteList extends Component {
     render() {
-        let palettes = this.props.palettes.map(palette => 
-        <Link to={`palette/${palette.id}`}>
-            <div className="link-content">
-                <span className="name">{palette.paletteName}</span>
-                <span className="emoji">{palette.emoji}</span>
-            </div>
-        </Link>)
+        //let palettes = this.props.palettes.map(palette => 
+        //<Link to={`palette/${palette.id}`}>
+        //    <div className="link-content">
+        //        <span className="name">{palette.paletteName}</span>
+        //        <span className="emoji">{palette.emoji}</span>
+        //    </div>
+        //</Link>)
+        let palettes = this.props.palettes.map(palette =>
+            <MiniPalette {...palette}/>)
         return (
-            <div>
-                {palettes}
+            <div className={this.props.classes.root}>
+                <div className={this.props.classes.container}>
+                    <nav className={this.props.classes.nav}>
+                        <h1>React Colors</h1>
+                    </nav>
+                    <div className={this.props.classes.palettes}>
+                        {palettes}
+                    </div>
+                </div>
             </div>
         )
     }
 }
+
+export default withStyles(styles)(PaletteList)
