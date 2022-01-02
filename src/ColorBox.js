7391d04a791c7a3111734d138124eda9c2bd7392
Ellie <ytaghavi@yahoo.com>
2022-01-01 17:29:29 -0800

    color-slider

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -3,14 +3,30 @@ import {CopyToClipboard} from 'react-copy-to-clipboard'
 import "./ColorBox.css"
 
 export default class ColorBox extends Component {
+    constructor(props){
+        super(props)
+        this.state = {copied:false}
+    }
+    
+    changeCopyState(){
+        this.setState({copied: true}, ()=>{
+            setTimeout(()=>this.setState({copied:false}), 1500);
+        })
+    }
+
     render() {
         const {name, color} = this.props;
         return (
-            <CopyToClipboard text={color}>
+            <CopyToClipboard onCopy={this.changeCopyState.bind(this)} text={color}>
                 <div key={color} className="color-box" style={{background: color}}>
+                    <div style={{background: color}} className={`copy-overlay ${this.state.copied && 'show'}`}/>
+                    <div className={`copy-msg ${this.state.copied && 'show'}`}>
+                        <h1>copied!</h1>
+                        <p>{color}</p>
+                    </div>
                     <div className="copy-container">
                         <div className="box-content">{name}</div>
-                        <button className="copy-button">copy</button>
+                        <button className="copy-button">{color}</button>
                     </div>
                     <span className="see-more">more</span>
                 </div>  
--- src/Palette.js
+++ src/Palette.js
@@ -1,12 +1,25 @@
 import React, { Component } from 'react'
 import ColorBox from './ColorBox.js'
 import "./Palette.css"
+import Slider, { Range } from 'rc-slider';
+import 'rc-slider/assets/index.css';
 
 export default class Palette extends Component {
+    constructor(props){
+        super(props)
+        this.state = {level:500};
+        this.changeLevel = this.changeLevel.bind(this);
+    }
+
+    changeLevel(level){
+        this.setState({level});
+    }
+
     render() {
-        let colorBoxes = this.props.colors.map(colorobj=> <ColorBox color={colorobj.color} name={colorobj.name}/>)
+        let colorBoxes = this.props.colors[this.state.level].map(colorobj=> <ColorBox color={colorobj.hex} name={colorobj.name}/>)
         return (
             <div className="palette">
+                <Slider step={100} min={100} max={900} onAfterChange={this.changeLevel}/>
                 <div className="palette-colors">{colorBoxes}</div>
             </div>
         )
